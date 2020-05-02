e71be4e44c1192f937f98f3a2b5da2cbc9c5be45
wangaibin <wangaibin@100tal.com>
2020-05-02 23:09:28 +0800

    init: 搭建基础项目架构

---
+++ .eslintrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  parser: "@typescript-eslint/parser",
+  plugins: ["@typescript-eslint"],
+  extends: ["alloy", "alloy/react", "alloy/typescript"],
+  rules: {
+    // 禁止使用 var
+    "no-var": "error",
+    // 优先使用 interface 而不是 type
+    "@typescript-eslint/consistent-type-definitions": ["error", "interface"],
+  },
+};
